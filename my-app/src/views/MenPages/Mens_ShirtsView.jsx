1982bd19445982505f7641c15a2818e731b5d223
George Petrov <NOBODYgosho@gmail.com>
2024-03-05 11:37:45 +0200

    making men-shirts with images
    Filled the db with the data

--- my-app/src/views/MenPages/Mens_ShirtsView.jsx
+++ my-app/src/views/MenPages/Mens_ShirtsView.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBar from '../../components/NavBar.jsx';
 import Footer from '../../components/FooterComponent.jsx';
 import MobileMenu from '../../components/MobileMenu.jsx';
+import ProductComponent from '../../components/ProductComponent.jsx';
 
 export default function MensShirts(){
 return(
     <>
     <MobileMenu />
     <NavBar />
+    <ProductComponent category="men-shirt"/>
     <h1>Mens Shirts Page</h1>
     <Footer />
     </>
