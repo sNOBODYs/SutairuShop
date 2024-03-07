62400bb19a63f6d857e71313211b6e5db905331d
George Petrov <NOBODYgosho@gmail.com>
2024-03-07 10:29:22 +0200

    Hoodiees products added

--- my-app/src/views/MenPages/Mens_HoodieView.jsx
+++ my-app/src/views/MenPages/Mens_HoodieView.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import NavBar from '../../components/NavBar.jsx';
 import Footer from '../../components/FooterComponent.jsx';
 import MobileMenu from '../../components/MobileMenu.jsx';
+import ProductComponent from '../../components/ProductComponent.jsx';
 
 export default function MensHoodie(){
 return(
     <>
     <MobileMenu />
     <NavBar />
-    <h1>Mens Hoodie Page</h1>
+    <section className ="page-header">
+        <h1>Mens Hoodies</h1>
+    </section>
+    <ProductComponent category="men-hoodie"/>
     <Footer />
     </>
 )
