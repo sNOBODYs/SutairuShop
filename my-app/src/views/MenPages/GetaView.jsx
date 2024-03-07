dab341dc3100842f0668e45a883f3606e82ea92b
George Petrov <NOBODYgosho@gmail.com>
2024-03-07 10:31:48 +0200

    Getas products added

--- my-app/src/views/MenPages/GetaView.jsx
+++ my-app/src/views/MenPages/GetaView.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import NavBar from '../../components/NavBar.jsx';
 import Footer from '../../components/FooterComponent.jsx';
 import MobileMenu from '../../components/MobileMenu.jsx';
+import ProductComponent from '../../components/ProductComponent.jsx';
 
 export default function MensGeta(){
 return(
     <>
     <MobileMenu />
     <NavBar />
-    <h1>Mens Geta Page</h1>
+    <section className ="page-header">
+        <h1>Mens Getas</h1>
+    </section>
+    <ProductComponent category="men-geta"/>
     <Footer />
     </>
 )
