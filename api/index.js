628b0ddc8b49de468d0dbfea27f9ca2ebb9a134a
George Petrov <NOBODYgosho@gmail.com>
2024-03-19 21:29:49 +0200

    create update user api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors'
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
@@ -18,6 +19,7 @@ app.use(cors({
     credentials: true,
 }));
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 })
