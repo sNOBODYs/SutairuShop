ec8b682241237919ae2a5a874abdffdac5964cac
George Petrov <NOBODYgosho@gmail.com>
2024-03-28 13:02:51 +0200

    update cart window

--- my-app/src/components/cartShowComponent.jsx
+++ my-app/src/components/cartShowComponent.jsx
@@ -6,14 +6,17 @@ import { getDownloadURL, ref, getStorage } from "firebase/storage";
 export default function CartShowComponent() {
     const currentCart = useSelector(state => state.cart.currentCart);
     const [cartItems, setCartItems] = useState([]);
-    const storage = getStorage(); 
+    const storage = getStorage();
 
     useEffect(() => {
         if (currentCart && currentCart.cart) {
             fetchProductImages(currentCart.cart);
         }
     }, [currentCart]);
-
+ 
+    function handleUpdate(params) {
+        
+    }
     const fetchProductImages = async (products) => {
         const updatedCartItems = [];
 
@@ -39,14 +42,25 @@ export default function CartShowComponent() {
                     </div>
                 </div>
                 <div className="scrolabble-container-products-cartmini">
-                {cartItems.map((item, index) => (
+                    {cartItems.map((item, index) => (
                         <div className="product-cartmini" key={index}>
                             <div className="product-cartmini-image">
-                            <img src={item.imageURL} alt={item.productName} />
+                                <img src={item.imageURL} alt={item.productName} />
                             </div>
-                            <p className='product-cartmini-name'>{item.productName}</p>
-                            <p className='product-cartmini-price'>{item.productPrice}</p>
-                            <p className='product-cartmini-size'>{item.productSize}</p>
+                            <div className="product-cartmini-col1">
+                                <p className='product-cartmini-name'>{item.productName}</p>
+                                <p className='product-cartmini-price'>${item.productPrice}.00</p>
+                                <div className="product-cartmini-size-container">
+                                <p className='product-cartmini-size'>Size:
+                                <select value={item.productSize} onChange={handleUpdate()}>
+                                    <option value="S">S</option>
+                                    <option value="M">M</option>
+                                    <option value="L">L</option>
+                                    <option value="XL">XL</option>
+                                    <option value="XXL">XXL</option>
+                                </select></p>
+                                </div>
+                            </div>
                             <p className='product-cartmini-quantity'>Quantity: {item.productQuantity}</p>
                         </div>
                     ))}
